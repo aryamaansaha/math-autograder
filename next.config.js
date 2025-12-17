1abc906a56413e6d923dc4d09c3373cbcff25036
Aryamaan Saha <aryamaansaha78@gmail.com>
2025-12-17 01:32:51 -0500

    Remove experimental server components configuration from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['@supabase/supabase-js'],
-  },
   webpack: (config) => {
     config.resolve.extensionAlias = {
       '.js': ['.ts', '.tsx', '.js', '.jsx'],
