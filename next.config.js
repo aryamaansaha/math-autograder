fb47d2bde059509ab0e9450a9385ff9fdee16144
Aryamaan Saha <aryamaansaha78@gmail.com>
2025-12-17 01:34:29 -0500

    Update next.config.js to include transpilePackages for Supabase and remove webpack extension alias configuration

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.resolve.extensionAlias = {
-      '.js': ['.ts', '.tsx', '.js', '.jsx'],
-    };
-    return config;
-  },
+  transpilePackages: ['@supabase/supabase-js', '@supabase/node-fetch'],
 }
 
 module.exports = nextConfig
